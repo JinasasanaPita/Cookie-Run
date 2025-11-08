3586da2d636438d67f1c1a4d66bdeb871a0b0ac8
Jinasasana Pita <p.jinasasana@gmail.com>
2025-11-08 16:34:18 +0800

    Initialised project
    Initialised project to Unity 2D URP project. Added card class and deck class

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Card
+{
+    string name;
+    string cost;
+    bool hasFlip;
+}
+
+public class CookieCard : Card
+{
+    int level;
+    int hp;
+}
+
+public class ItemCard : Card
+{
+
+}
+
+public class TrapCard : Card
+{
+
+}
+
+public class StageCard : Card
+{
+    
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Deck
+{
+    List<Card> deck;
+    int max_size = 60;
+}
